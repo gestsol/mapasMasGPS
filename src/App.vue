b51d6380f8754b8d0a39ab09c4e6d03a3ae62d47
ivalenzuela <ivalenzuela@ntic.cl>
2023-09-05 14:37:55 -0300

    Los siguientes cambios se han realizado en el archivo App.vue:
    - Se ha añadido un componente Mapa.
    - Se ha añadido un select para seleccionar un bus.
    - Se ha realizado una petición a una API para obtener una lista de buses.
    - Se ha implementado la funcionalidad para seleccionar un bus.

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,61 @@
 <template>
-
   <div class="container">
-  
-    <Mapa  :selectedTracker="selectedTracker" :selectedPatente="selectedPatente" :selectedSource="selectedSource"></mapa>
-  
-      <select class="form-select mt-2" v-model="selectedTracker" @change="selectBus">
-          <option v-for="bus in buses" :value="bus.id" :key="bus.label">
-            {{ bus.label }}
-          </option>
-        </select>
-  
-      </div>
-  
-  
-  </template>
-  
-  <script>
-  import Mapa from './components/Mapa.vue';
-  import axios from "axios";
-  
-  
-  
-  
-  
-  export default {
-    name: 'App',
-    components: {
-      Mapa,
-     
+    <Mapa :selectedTracker="selectedTracker" :selectedPatente="selectedPatente" :selectedSource="selectedSource"></Mapa>
+
+    <select class="form-select mt-2" v-model="selectedTracker" @change="selectBus">
+      <option v-for="bus in buses" :value="bus.id.toString()" :key="bus.label">
+        {{ bus.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+import Mapa from './components/Mapa.vue';
+import axios from "axios";
+import { reactive } from "vue";
+
+export default {
+  name: 'App',
+  components: {
+    Mapa,
+  },
+  data() {
+    return {
+      buses: [],
+      selectedPatente: '',
+      selectedSource: 10103058,
+      selectedTracker: ''
+    }
+  },
+  created() {
+    this.fetch();
+  },
+  methods: {
+    selectBus() {
+      const selectedBus = this.buses.find(bus => bus.id === parseInt(this.selectedTracker));
+      this.selectedSource = selectedBus.source.id;
+      this.selectedPatente = selectedBus.label;
+      console.log(selectedBus);
     },
-    data() {
-        return {
-          buses: [
-            
-          ],
-         
-          selectedPatente: '',
-          selectedSource: 10103058,
-          selectedTracker: ''
-        }
-      },
-    
-    
-      
-      created (){ 
-           this.fetch ()  
-          },
-    
-      methods: {
-  
-        selectBus() {
-          const selectedBus = this.buses.find(bus => bus.id === this.selectedTracker);
-          this.selectedSource = selectedBus.source.id;
-          this.selectedPatente = selectedBus.label;
-        },
-  
-  
-  
-         async fetch (){
-    
-        
-          try {  
-    
-            const resultado= await axios.post ("http://www.trackermasgps.com/api-v2/tracker/list",
-          {"hash": "a42aea049190363eb6e21ecc954600b6"});
-    
-          console.log(resultado.data.list);
-    
-          this.buses =resultado.data.list ;
-    
-          }catch (error){
-    
-            console.log(error);
-    
-    
-          }
-         }
-      },
-  
+    async fetch() {
+      try {
+        const resultado = await axios.post("http://www.trackermasgps.com/api-v2/tracker/list", { "hash": "a42aea049190363eb6e21ecc954600b6" });
+        this.buses = resultado.data.list;
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
-  </script>
-  
-  <style>
-  body {
-    background: rgb(163, 155, 155);
-  }
-  
-  .form-select {
-    width: 50%;
-  }
-  
-  
-  
-  </style>
-  
+}
+</script>
+
+<style>
+body {
+  background: rgb(163, 155, 155);
+}
+
+.form-select {
+  width: 50%;
+}
+</style>
