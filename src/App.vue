f44a891265d8c02669da1ea63c56b98f4e1d0349
Dorian <dorian.celu@gmail.com>
2023-09-05 12:43:38 -0300

    Selector

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,96 @@
 <template>
 
-<Mapa></mapa>
-  <div>
-      <select class="form-select" v-model="selectedItem"> 
-      <option v-for="(item,index) in buses" :key="index" :value="item.id">{{ item.label }}</option>
-      </select>
-    </div>
-
-
-</template>
-
-<script>
-import Mapa from './components/Mapa.vue';
-import axios from "axios";
-
-
-
-
-
-export default {
-  name: 'App',
-  components: {
-    Mapa,
-   
-  },
-  data() {
-      return {
-        buses: [
-          
-        ],
-        selectedItem: null
-      }
+  <div class="container">
+  
+    <Mapa  :selectedTracker="selectedTracker" :selectedPatente="selectedPatente" :selectedSource="selectedSource"></mapa>
+  
+      <select class="form-select mt-2" v-model="selectedTracker" @change="selectBus">
+          <option v-for="bus in buses" :value="bus.id" :key="bus.label">
+            {{ bus.label }}
+          </option>
+        </select>
+  
+      </div>
+  
+  
+  </template>
+  
+  <script>
+  import Mapa from './components/Mapa.vue';
+  import axios from "axios";
+  
+  
+  
+  
+  
+  export default {
+    name: 'App',
+    components: {
+      Mapa,
+     
     },
-  
-  
+    data() {
+        return {
+          buses: [
+            
+          ],
+         
+          selectedPatente: '',
+          selectedSource: 10103058,
+          selectedTracker: ''
+        }
+      },
     
-    created (){ 
-         this.fetch ()  
+    
+      
+      created (){ 
+           this.fetch ()  
+          },
+    
+      methods: {
+  
+        selectBus() {
+          const selectedBus = this.buses.find(bus => bus.id === this.selectedTracker);
+          this.selectedSource = selectedBus.source.id;
+          this.selectedPatente = selectedBus.label;
         },
   
-    methods: {
-       async fetch (){
-  
-      
-        try {  
-  
-          const resultado= await axios.post ("http://www.trackermasgps.com/api-v2/tracker/list",
-        {"hash": "a42aea049190363eb6e21ecc954600b6"});
-  
-        console.log(resultado.data.list);
-  
-        this.buses =resultado.data.list ;
-  
-        }catch (error){
-  
-          console.log(error);
   
   
-        }
-       }
-    },
-
-}
-</script>
-
-<style>
-
-</style>
+         async fetch (){
+    
+        
+          try {  
+    
+            const resultado= await axios.post ("http://www.trackermasgps.com/api-v2/tracker/list",
+          {"hash": "a42aea049190363eb6e21ecc954600b6"});
+    
+          console.log(resultado.data.list);
+    
+          this.buses =resultado.data.list ;
+    
+          }catch (error){
+    
+            console.log(error);
+    
+    
+          }
+         }
+      },
+  
+  }
+  </script>
+  
+  <style>
+  body {
+    background: rgb(163, 155, 155);
+  }
+  
+  .form-select {
+    width: 50%;
+  }
+  
+  
+  
+  </style>
+  
--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -1,66 +1,163 @@
 <template>
-  <h1>mapa</h1>
+    <div>
+      <div class="card mt-1 " >
+        <div class="card-header">
+         <b> Mi BUS</b>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{{ selectedPatente }} - {{ selectedSource }} - {{ selectedTracker }}</h5>
+        
+            <p> La distancia es: <b>{{ distance }}</b> km </p>
+      <p> El tiempo estimado de espera es de: <b>{{ demora }} </b> min </p>
+      <span>{{ source }}</span>
+    
+        </div>
+      </div>
+
+
+    
+  <h6>
+    
+  </h6>
+  </div>
+
+  
   <GMapMap
-      :center=mapCenter
-      :zoom=mapZoom
-      map-type-id="roadmap"
-      style="width: 100vw; height: 500px"
+    :center=mapCenter
+    :zoom=mapZoom
+    map-type-id="roadmap"
+    style="width: 100%; height: 500px"
   >
 
-    <GMapMarker :v-model="positionBus" :position="positionBus"></GMapMarker>
+<GMapMarker  :position="positionBus"
 
-  </GMapMap>
+></GMapMarker>
+
+<GMapMarker  :position="Paradero"></GMapMarker>        
+     
+</GMapMap>
 
 </template>
 
 <script>
-//import { GmapMap, GmapMarker } from '@fawmi/vue-google-maps';
-//import { io } from 'socket.io-client';
-//import { ref, onMounted, onUnmounted,  watchEffect } from "vue";
+
+import { ref, onMounted, onUnmounted,  watchEffect } from "vue";
+
 export default {
   name: 'Mapa',
   data() {
     return {
-      mapCenter: {lat: -33.4132183, lng: -70.5406616}, // Coordenadas del centro del mapa
-      mapZoom: 10, // Nivel de zoom del mapa
+      mapCenter: { lat: -33.4132183, lng: -70.5406616 }, // Coordenadas del centro del mapa
+      mapZoom: 14, // Nivel de zoom del mapa
       coordenadas: {},
-      positionBus:  {lat: -33.4132183, lng: -70.5406616},
-      trackers: 10176445
+      source: null,
+      positionBus: { lat: -33.4132183, lng: -70.5406616 },
+      Paradero: { lat: -33.4132183, lng: -70.5406616},
+      trackers: [10176497,10176427,10105327,10176442,10176445,10177113,10176440,10176496,10177113],
 
+      distance: null,
+      demora: null,
+      selectedOption:"",
+      selectedSource:0,  
+      
     }
+
   },
-  mounted() {
-    let socket = new WebSocket("wss://masgps.witservices.io/api-v2/event/subscription");
-    let message = {
-      "action": "subscribe",
-      "hash": "a42aea049190363eb6e21ecc954600b6",
-      "events": ["state"],
-      "trackers": [this.trackers]
-    };
 
-    socket.onopen = (e) => {
-      socket.send(JSON.stringify(message));
-    };
+  props: {
+ 
+    selectedSource:{
+      type: Number,
+      //default :'10103062',
+      required: true
+      
+    },
+    selectedPatente:{
+      type: String,
+    required:true},
 
-    socket.onmessage = (event) => {
-      const tramaGps = JSON.parse(event.data);
+    selectedTracker:{
+      type: String,
+      required: true,}
 
-      if (tramaGps.data.hasOwnProperty('gps')) {
-        this.positionBus = tramaGps.data.gps.location;
-        this.coordenadas = tramaGps.data.gps.location;
-        this.mapCenter = tramaGps.data.gps.location;
-        let tracker = tramaGps.data.source_id;
+  },
+  
+  methods :{
 
-        console.log(this.coordenadas);
-        console.log(tracker);
+    conectar :function () {
+
+      let socket = new WebSocket("wss://masgps.witservices.io/api-v2/event/subscription");
+      let message = { "action": "subscribe", "hash": "a42aea049190363eb6e21ecc954600b6", "events": ["state"], "trackers": this.trackers };
+
+      socket.onopen = (e) => {
+
+        socket.send(JSON.stringify(message));
+      };
+      socket.onmessage = (event) => {
+
+        const tramaGps = JSON.parse(event.data);
+          //
+        if (tramaGps.data.hasOwnProperty('gps')) {
+
+          console.log (tramaGps.data.source_id);
+
+          if (tramaGps.data.source_id ==  '10105327' ) {
+
+            function degToRad(deg) {
+              return deg * (Math.PI / 180);
+            }
+            this.positionBus = tramaGps.data.gps.location;
+            this.source = tramaGps.data.source_id;
+            this.mapCenter = tramaGps.data.gps.location;
+           
+
+            let lat1 = tramaGps.data.gps.location.lat;
+            let lon1 = tramaGps.data.gps.location.lng;
+
+            let lat2 = this.Paradero.lat;
+            let lon2 = this.Paradero.lng;
+
+            const radioTierra = 6371; // Radio de la Tierra en kilómetros
+            const dLat = degToRad(lat2 - lat1);
+
+            const dLon = degToRad(lon2 - lon1);
+
+            const a =
+              Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            const distancia = radioTierra * c;
+
+            //distancia.toFixed(2); // Devuelve la distancia redondeada a 2 decimales
+
+            this.distance = distancia.toFixed(2);
+
+            const tiempo = distancia * 3;
+
+            this.demora = tiempo.toFixed(2);
+
+            console.log(tramaGps);
+            console.log(this.source);
+          }
+      
+        }
       }
-    };
+    }
+    
+  } , 
+
+  mounted (){
+    this.conectar();
   },
+ 
 
+  }
 
-}
-
+ 
 </script>
+
 
 
 <style>
